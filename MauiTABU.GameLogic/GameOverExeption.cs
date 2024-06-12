863ab0346a0e8a6e9be05964c8e0dead85f606ba
Lena Eckstein <leckstein@schueler.hakzell.at>
2024-06-12 11:02:58 +0200

    Game Logic

---
+++ MauiTABU.GameLogic/GameOverExeption.cs
@@ -0,0 +1,24 @@
+﻿using System.Runtime.Serialization;
+
+namespace MauiTABU.GameLogic
+{
+    [Serializable]
+    internal class GameOverExeption : Exception
+    {
+        public GameOverExeption()
+        {
+        }
+
+        public GameOverExeption(string? message) : base(message)
+        {
+        }
+
+        public GameOverExeption(string? message, Exception? innerException) : base(message, innerException)
+        {
+        }
+
+        protected GameOverExeption(SerializationInfo info, StreamingContext context) : base(info, context)
+        {
+        }
+    }
+}
---
+++ MauiTABU.GameLogic/WordChecker.cs
@@ -0,0 +1,34 @@
+﻿namespace MauiTABU.GameLogic;
+
+
+public class WordChecker 
+{
+
+    List<string> _notUsableWords;
+    public WordChecker(List<string> notUseableWords)
+    {
+        _notUsableWords = notUseableWords;
+    }
+
+    public bool CheckWord(string word)
+    {
+        if (_notUsableWords.Contains(word))
+        {
+            // wenn etw. aus der Liste hergenommen wird
+            return false;
+        }
+        
+        else if (word == "scheiße") // das ist absolut verboten
+        {
+            throw new GameOverExeption("This Word is not allowed!");
+        }
+        
+        // sagt das throw new das Objekt das dann hinter
+        // geschrieben ist eine Exeption ist und macht dann eine
+        // eigene Exeption weil er es selber checkt
+        
+        return true;
+    }
+
+
+}
