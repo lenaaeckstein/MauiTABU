6c673d6270bc31bf54602c618f9e2c7533c14a16
Lena Eckstein <leckstein@schueler.hakzell.at>
2024-06-12 11:08:15 +0200

    add mvvm Packet

---
+++ MauiTABU.Gui/ViewModels/GameViewModels.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MauiTABU.Gui.ViewModels
+{
+    internal class GameViewModels
+    {
+    }
+}
