78c3d3f3f699ea0cb01d4b6bcbd2673c7e25c7fb
Lena Eckstein <leckstein@schueler.hakzell.at>
2024-06-12 10:46:22 +0200

    gitignore

---
+++ MauiTABU.Gui/App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace MauiTABU.Gui
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new AppShell();
+        }
+    }
+}
---
+++ MauiTABU.Gui/AppShell.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace MauiTABU.Gui
+{
+    public partial class AppShell : Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+        }
+    }
+}
